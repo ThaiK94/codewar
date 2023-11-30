2fe4929c8894dbff627d4fd68e0315b9a98a6160
Thai Khong <thaik.dev@gmail.com>
2023-11-29 17:59:31 -0800

    codewar9

--- codewar_8/js/main.js
+++ codewar_8/js/main.js
@@ -1,6 +1,19 @@
-Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
-Examples:
+// Examples:
 
-solution('abc', 'bc') // returns true
-solution('abc', 'd') // returns false
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending){
+  // TODO: complete
+  
+  if(str.substr(str.length-ending.length) === ending)
+    {
+      return true
+    }
+  else
+    {
+      return false
+    }
+}
